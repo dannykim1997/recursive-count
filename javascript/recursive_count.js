78ca336c72de98297a1fa72045cf8fdbb198e4f4
github username <github email address>
2021-03-15 13:20:02 -0700

    done

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,19 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num >= 10) {
+    return;
+  }
+
+  console.log(num);
+  recursiveCount(num + 1);
+
+  // THIS IS ALSO A VALID OPTION INSTEAD OF THE ABOVE
+  // recursiveCount(++num);
+
+  // THIS WILL CAUSE A STACK OVERFLOW. WHY?
+  // recursiveCount(num++);
+
+  // IF YOU LOG NUM BELOW, THE NUMBERS PRINT BACKWARDS FROM 9 TO 0. WHY?
+  // console.log(num);
 }
 
 if (require.main === module) {
